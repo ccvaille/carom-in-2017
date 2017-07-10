1f2e238bb88447b5709eeb8f2266e7228b4df6de
dingchunyan <dingchunyan@ecqun.com>
2017-07-10 22:10:57 +0800

    commit

---
+++ Router-demo/readme.md
@@ -0,0 +1,6 @@
+## Router-demo
+- 初始化
+    - npm init 
+- 安装
+    - npm install --save react react-dom react-router
+    - npm install --save-dev babel-core babel-eslint babel-loader babel-preset-es2015 babel-preset-react eslint eslint-config-airbnb eslint-loader eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react webpack webpack-dev-server html-webpack-plugin
---
+++ Router-demo/src/components/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router,Route,hashHistory,IndexRoute} from 'react-router';
+import App from './components/App';
+import Home from './components/Home';
+import Repos from './components/Repos';
+import About from './components/About';
+import User from './components/User';
+import Contacts from './components/Contacts';
