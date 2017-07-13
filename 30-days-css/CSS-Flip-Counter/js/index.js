d3826efeedf0e4f9bb72eb603614d548cfd61616
dingchunyan <dingchunyan@ecqun.com>
2017-07-13 21:00:08 +0800

    commit CSS-Flip-Counter

--- 30-days-css/CSS-Flip-Counter/js/index.js
+++ 30-days-css/CSS-Flip-Counter/js/index.js
@@ -1,21 +1,14 @@
 $('document').ready(function() {
     num = parseInt($('.hinge .top').text());
+
     function add() {
         num++;
-        var un = num + 1 ;
         if (num > 9) {
             num = 0;
         }
-        if (un > 9) {
-            un = 0;
-        }
-        $('.unbefore,.unback').text(un);
-        $('.hinge .top').text(num);
-        $('.hinge .bottom').text(un);
+        $('.unbefore,.unback').text(num);
+        $('.hinge .top,.hinge .bottom').text(num);
     }
-
-    // $('.time-box').click(function() {
-        $('.time-box li').addClass('ani');
-        var time = setInterval(add, 1000);
-    // })
+    $('.time-box li').addClass('ani');
+    var time = setInterval(add, 1000);
 })
